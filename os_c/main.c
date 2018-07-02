2a7b9aa6af6e1d3761578ed7a2cf5646604a7b49
zhuangyuanfeng <zhuangyuanfeng@qbb6.com>
2018-07-02 10:49:41 +0800

    Initial Commit

---
+++ os_c/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  os_c
+//
+//  Created by mac_zyf on 2018/7/2.
+//  Copyright © 2018年 mac_zyf. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
